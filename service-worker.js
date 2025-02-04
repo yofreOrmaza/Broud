449706b6e7d94d504187d12858756c41300f7ab4
yofreOrmaza <ormaza.main@gmail.com>
2025-02-04 17:38:33 -0500

    style(logo, brand name): Reemplazar directorio logos PWA y actualizar nombre de app

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,13 @@
-const CACHE_NAME = "OFF-2R-v1";
+const CACHE_NAME = "Broud-v1";
 const urlsToCache = [
   "/",
   "/index.html",
   "/css/styles.css",
   "/js/script.js",
-  "/data/img/iconsAPP/Android/OFF2RFAVICON-192x192.jpg",
-  "/data/img/iconsAPP/Android&Browser/OFF2RFAVICON-512x512.jpg",
-  "/data/img/iconsAPP/iOS/OFF2RFAVICON-180x180.jpg"
+  "/data/img/favicon/favicon/web-app-manifest-192x192.png",
+  "/data/img/favicon/favicon/web-app-manifest-512x512.png",
+  "/data/img/favicon/broudFORpwa.png",
+  "/data/img/favicon/broudFORpwaNightMode.png"
 ];
 
 // Instalar el Service Worker
